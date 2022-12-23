c3b471ff18f7a19162f625f696fe7f27c9487885
tarrantinogit <lestd3v3lop@gmail.com>
2022-12-23 16:17:44 +0200

    fixed lags

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	scroll();
 	testimonilasSwiper;
 	accordion();
-	popup('button[data-popup-contact]', 'img[data-popup-contact-close]', '.popup-contact');
+	popup('a[data-popup-contact]', 'img[data-popup-contact-close]', '.popup-contact');
 	burgerMenu();
 	capabilitiesSwiper();
 	downloadButton();
