986f505381f29f3ec818823304f411e8f962b71a
Aralov Mikhail <aralov.mykhailo@gmail.com>
2022-12-28 19:49:43 +0200

    add marquee marquee-desc

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
 import {
 	testimonilasSwiper,
-	popup,
 	burgerMenu,
 	accordion,
-	downloadButton, technologies, capabilitiesSwiper, symbolsCounter, scroll, inputsValidation,
+	downloadButton, capabilitiesSwiper, symbolsCounter, scroll, inputsValidation,
 
 } from "./modules/helpers.js";
 
@@ -14,11 +13,35 @@ document.addEventListener('DOMContentLoaded', () => {
 	scroll();
 	testimonilasSwiper;
 	accordion();
-	popup('a[data-popup-contact]', 'img[data-popup-contact-close]', '.popup-contact');
 	burgerMenu();
 	capabilitiesSwiper();
 	downloadButton();
-	technologies();
 	symbolsCounter();
 	inputsValidation();
+
+	const techSectionRef = document.querySelector('#technologies');
+
+	const techMarque = $('.marquee').marquee({
+		duration: 7000,
+		delayBeforeStart: 0,
+		direction: 'left',
+		duplicated: true,
+		startVisible: true,
+		pauseOnHover: true
+	});
+
+	techMarque.marquee('pause');
+
+	const callback = function (entries) {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				techMarque.marquee('resume')
+			} else {
+				techMarque.marquee('pause');
+			}
+		})
+	};
+	const observer = new IntersectionObserver(callback, { threshold: 0 });
+	observer.observe(techSectionRef);
+
 })
--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,26 +1,6 @@
 import Swiper from "swiper/bundle";
 
 
-export function throttle(callee, timeout) {
-
-	let timer = null
-
-	return function perform(...args) {
-
-		if (timer) return
-
-
-		timer = setTimeout(() => {
-
-			callee(...args)
-
-
-			clearTimeout(timer)
-			timer = null
-		}, timeout)
-	}
-}
-
 export function downloadButton() {
 	const downloadButtons = document.querySelectorAll('.input--file');
 	const fileChosen = document.querySelectorAll('.download__text');
@@ -152,58 +132,6 @@ export function accordion() {
 	});
 }
 
-
-export function popup(btnSelector, btnCloseSelector, popupSelector) {
-	const overlay = document.querySelector('.overlay');
-	const btnOpen = document.querySelector(btnSelector);
-	const popup = document.querySelector(popupSelector);
-	const btnClose = document.querySelector(btnCloseSelector);
-	const mobileMenu = document.querySelector('.header__burger-menu');
-
-
-	if (document.querySelector('a[data-contact-us-close]')) {
-		const mobileCloseBtn = document.querySelector('a[data-contact-us-close]');
-
-		mobileCloseBtn.addEventListener('click', (e) => {
-			if (e.target) {
-				e.preventDefault();
-				popup.classList.remove('active');
-				overlay.classList.remove('active');
-				document.body.style.overflowY = 'visible';
-			}
-		});
-	}
-
-	btnOpen.addEventListener('click', (e) => {
-		if (e.target) {
-			e.preventDefault();
-			document.body.style.overflowY = 'hidden';
-			popup.classList.add('active');
-			overlay.classList.add('active')
-		}
-
-		btnClose.addEventListener('click', (e) => {
-			if (e.target && mobileMenu.classList.contains('active')) {
-				document.body.style.overflowY = 'visible';
-				popup.classList.remove('active');
-			} else {
-				overlay.classList.remove('active');
-				popup.classList.remove('active');
-				document.body.style.overflowY = 'visible';
-			}
-		})
-
-		overlay.addEventListener('click', (e) => {
-			if (e.target) {
-				popup.classList.remove('active');
-				overlay.classList.remove('active');
-				document.body.style.overflowY = 'visible';
-			}
-		})
-	})
-}
-
-
 export function burgerMenu() {
 	const burgerBtn = document.querySelector('.header__burger');
 	const mobileMenu = document.querySelector('.header__burger-menu');
@@ -242,78 +170,6 @@ export function burgerMenu() {
 			document.body.style.overflowY = 'visible';
 		}
 	});
-}
-
-
-export function technologies() {
-	const tech = document.querySelector(".technologies");
-	if (!tech) return null
-	let oneRow = document.querySelector(".technologies__inner-first");
-	let twoRow = document.querySelector(".technologies__inner-second");
-	let test2Inner = document.querySelector(".technologies__inner");
-	let test2WrapperWidth = document.querySelector(".technologies__items").offsetWidth;
-	let test2InnerWidth = document.querySelector(".technologies__inner").offsetWidth;
-	let oneRowWidth = document.querySelector(".technologies__inner-first").offsetWidth;
-	let twoRowWidth = document.querySelector(".technologies__inner-second").offsetWidth;
-	let trigger2ContainerOfTop = document.querySelector(".technologies__trigger").getBoundingClientRect().top + scrollY;
-	let trigger2Height = document.querySelector(".technologies__trigger").offsetHeight;
-	let scrollPercent2Container = (scrollY - trigger2ContainerOfTop + window.innerHeight) / (window.innerHeight + trigger2Height);
-	let windowInnerWidth = window.innerWidth;
-
-	function updateParams(e) {
-		oneRow.style.transform = null;
-		twoRow.style.transform = null;
-		test2Inner.style.transform = null;
-		test2WrapperWidth = document.querySelector(".technologies__items").offsetWidth;
-		oneRowWidth = document.querySelector(".technologies__inner-first").offsetWidth;
-		twoRowWidth = document.querySelector(".technologies__inner-second").offsetWidth;
-		test2InnerWidth = document.querySelector(".technologies__inner").offsetWidth;
-		trigger2ContainerOfTop = document.querySelector(".technologies__trigger").getBoundingClientRect().top + scrollY;
-		trigger2Height = document.querySelector(".technologies__trigger").offsetHeight;
-		scrollPercent2Container = (scrollY - trigger2ContainerOfTop + window.innerHeight) / (window.innerHeight + trigger2Height);
-		windowInnerWidth = window.innerWidth;
-	}
-
-	function scrollPage() {
-		trigger2ContainerOfTop = document.querySelector(".technologies__trigger").getBoundingClientRect().top + scrollY;
-		trigger2Height = document.querySelector(".technologies__trigger").offsetHeight;
-		scrollPercent2Container = (scrollY - trigger2ContainerOfTop + window.innerHeight) / (window.innerHeight + trigger2Height);
-
-
-		if (scrollPercent2Container >= 0 && scrollPercent2Container <= 1) {
-			if (windowInnerWidth < 992) {
-				oneRow.style.cssText = `transform: translateX(-${(oneRowWidth - test2WrapperWidth) * scrollPercent2Container}px );`;
-				twoRow.style.cssText = `transform: translateX(${(twoRowWidth - test2WrapperWidth) * scrollPercent2Container}px );`;
-			} else {
-				test2Inner.style.cssText = `transform: translateX(-${(test2InnerWidth - test2WrapperWidth) * scrollPercent2Container}px );`;
-			}
-		}
-
-		if (scrollPercent2Container < 0) {
-			if (windowInnerWidth <= 993) {
-				oneRow.style.cssText = `transform: translateX(0px );`;
-				twoRow.style.cssText = `transform: translateX(0px );`;
-			} else {
-
-				test2Inner.style.cssText = `transform: translateX(0px );`;
-			}
-		}
-		if (scrollPercent2Container > 1) {
-			if (windowInnerWidth <= 993) {
-				oneRow.style.cssText = `transform: translateX(-${oneRowWidth - test2WrapperWidth}px );`;
-				twoRow.style.cssText = `transform: translateX(${twoRowWidth - test2WrapperWidth}px );`;
-			} else {
-				test2Inner.style.cssText = `transform: translateX(-${test2InnerWidth - test2WrapperWidth}px );`;
-			}
-		}
-	}
-
-	const throttleScrollPage = throttle(scrollPage, 300)
-	const throttleUpdateParams = throttle(updateParams, 300)
-
-	window.addEventListener("scroll", throttleScrollPage);
-	window.addEventListener("resize", throttleUpdateParams);
-
 }
 
 
