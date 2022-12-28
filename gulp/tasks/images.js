16ec79e20ea3335845bdb7397fbc3c78f446af06
Aralov Mikhail <aralov.mykhailo@gmail.com>
2022-12-28 17:02:37 +0200

    minor ui fixes

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,4 +1,3 @@
-import webp from 'gulp-webp'
 import imagemin from "gulp-imagemin"
 
 export const images = () => {
@@ -10,7 +9,6 @@ export const images = () => {
       })
     ))
     .pipe(app.plugins.newer(app.path.build.images))
-    .pipe(app.plugins.if(app.isBuild, webp()))
     .pipe(app.plugins.if(app.isBuild, app.gulp.dest(app.path.build.images)))
     .pipe(app.plugins.if(app.isBuild, app.gulp.src(app.path.src.images)))
     .pipe(app.plugins.if(app.isBuild, app.plugins.newer(app.path.build.images)))
